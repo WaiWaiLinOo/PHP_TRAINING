ac1a35d0ad299ba09fb417aa005c06ba33a8a3ca
wailinoo <wailinoo.august@gmail.com>
2021-12-16 10:39:48 +0630

    [master]finished php tutorial3

---
+++ tutorials/tutorial_04/_actions/login.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$name = $_POST['name'];
+$password = $_POST['password'];
+if ($name === 'wailinoo' and $password === '123') {
+  $_SESSION['user'] = ['username' => 'John Doe'];
+  header('location: ../profile.php');
+} else {
+  header('location: ../index.php?incorrect=1');
+}
