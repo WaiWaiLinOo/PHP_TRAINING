d05ee33d3baf386b61824d9104d874e4df3648b9
wailinoo <wailinoo.august@gmail.com>
2021-12-16 14:35:09 +0630

    [master]finished php tutorial4

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -2,42 +2,42 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tutorial_02</title>
-  <style>
-    table {
-      border: 1px solid black;
-    }
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial_01</title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
 
-    td {
-      height: 50px;
-      width: 50px;
-    }
+        td {
+            height: 50px;
+            width: 50px;
+        }
 
-    .black {
-      background-color: black;
-    }
-  </style>
+        .black {
+            background-color: black;
+        }
+    </style>
 </head>
 
 <body>
-  <table>
-    <?php
-    for ($rows = 1; $rows <= 8; $rows++) {
-      echo "<tr>";
-      for ($columns = 1; $columns <= 8; $columns++) {
-        if (($rows + $columns) % 2 == 0) {
-          echo "<td class='black'></td>";
-        } else {
-          echo "<td></td>";
+    <table>
+        <?php
+        for ($rows = 1; $rows <= 8; $rows++) {
+            echo "<tr>";
+            for ($columns = 1; $columns <= 8; $columns++) {
+                if (($rows + $columns) % 2 == 0) {
+                    echo "<td class='black'></td>";
+                } else {
+                    echo "<td></td>";
+                }
+            }
+            echo "</tr>";
         }
-      }
-      echo "</tr>";
-    }
-    ?>
-  </table>
+        ?>
+    </table>
 </body>
 
 </html>
