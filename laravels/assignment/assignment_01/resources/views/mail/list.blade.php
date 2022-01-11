558d6c7d9cae906849c574daf404353dcd680f32
wailinoo <wailinoo.august@gmail.com>
2022-01-11 11:59:00 +0630

    [master] finished laravel assignment_05 mail and review update

--- laravels/assignment/assignment_01/resources/views/mail/list.blade.php
+++ laravels/assignment/assignment_01/resources/views/mail/list.blade.php
@@ -1,18 +1,53 @@
+
 @component('mail::message')
 # Student List
+<!DOCTYPE html>
+<html>
 
-@component('mail::table')
-| Name | Email | Major |
-| ------------- |:-------------:| --------:|
-@foreach ($students as $student)
-| {{$student->name}} | {{$student->email}} | {{$student->major_name}} |
-@endforeach
-@endcomponent
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
 
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+
+<body>
+
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Major</th>
+    </tr>
+    @component('mail::table')
+    @foreach ($students as $student)
+    <tr>
+      <td>{{$student->name}} </td>
+      <td>{{$student->email}} </td>
+      <td>{{$student->major_name}}</td>
+    </tr>
+    @endforeach
+    @endcomponent
+   </table>
+</body>
+</html>
 @component('mail::button', ['url' => 'http://localhost:8000/students'])
 See More Students
 @endcomponent
-
 Thanks,<br>
 Assginment student list
 @endcomponent
